84bf08634665602e1d1428f5dec142d8f9e09520
Lori Corbani <Lori.Corbani@jax.org>
2008-03-26 11:51:38 +0000

    TR8769

---
+++ abaload.sh
@@ -0,0 +1,149 @@
+#!/bin/sh 
+
+#
+# Program: abaload.sh
+#
+# Original Author: Lori Corbani
+#
+# Purpose:
+#
+# Job Stream for Allen Brain Atlas load
+#
+#	1.  Copy ABA file from downloads directory
+#	2.  From file, create association loader file
+#	3.  Load the Marker/ABA associations into MGI using the association loader
+#
+# History:
+#
+# 03/25/2008	lec
+#	- TR 8769
+#
+
+cd `dirname $0`
+LOG=`pwd`/abaload.log
+rm -rf ${LOG}
+
+# true if input from radar, false if from command line
+RADAR_FLAG=0
+
+#
+# Verify arguments to shell script
+#
+if [ $# -eq 1 ]
+then
+    APP_INFILE=$1
+    RADAR_FLAG=0
+elif [ $# -ne 0 ]
+then
+    echo "Usage: $0 [load file]" | tee -a ${LOG}
+    exit 1
+fi
+
+CONFIG_LOAD=`pwd`/abaload.config
+
+#
+# verify & source the configuration file
+#
+
+if [ ! -r ${CONFIG_LOAD} ]
+then
+    echo "Cannot read configuration file: ${CONFIG_LOAD}"
+    exit 1
+fi
+
+. ${CONFIG_LOAD}
+
+#
+#  Source the DLA library functions.
+#
+
+if [ "${DLAJOBSTREAMFUNC}" != "" ]
+then
+    if [ -r ${DLAJOBSTREAMFUNC} ]
+    then
+        . ${DLAJOBSTREAMFUNC}
+    else
+        echo "Cannot source DLA functions script: ${DLAJOBSTREAMFUNC}" | tee -a ${LOG}
+        exit 1
+    fi
+else
+    echo "Environment variable DLAJOBSTREAMFUNC has not been defined." | tee -a ${LOG}
+    exit 1
+fi
+
+#
+#  Fetch files to be loaded if not specified
+#
+if [ "${APP_INFILE}" = "" ]
+then
+    echo "\n`date`" >> ${LOG_DIAG}
+    echo "Fetching files to load..." >> ${LOG_DIAG}
+    APP_INFILE=`${RADAR_DBUTILS}/bin/getFilesToProcess.csh ${RADAR_DBSCHEMADIR} ${JOBSTREAM} ABA`
+    STAT=$?
+
+    if [ ${STAT} -ne 0 ]
+    then
+       echo "getFilesToProcess.csh failed. Return status: ${STAT}" | tee -a ${LOG_DIAG} ${LOG_PROC}
+       exit 1
+    fi
+fi
+
+#
+# createArchive including OUTPUTDIR, startLog, getConfigEnv, get job key
+#
+preload ${OUTPUTDIR}
+
+#
+# rm all files/dirs from INPUTDIR and OUTPUTDIR
+#
+cleanDir ${INPUTDIR} ${OUTPUTDIR}
+
+# if no input files report and shutdown gracefully
+if [ "${APP_INFILE}" = "" ]
+then
+    echo "No files to process" | tee -a ${LOG_DIAG} ${LOG_PROC}
+    shutDown
+    exit 0
+fi
+
+#
+# generate association file
+#
+
+cd ${INPUTDIR}
+cp ${APP_INFILE} .
+${ABALOAD}/abaparse.py
+STAT=$?
+checkStatus ${STAT} "${ABALOAD}/abaparse.py"
+
+#
+# run association load
+#
+echo "Running ABA association load" >> ${LOG_DIAG}
+${ASSOCLOAD}/bin/AssocLoad.sh ${CONFIG_LOAD} ${JOBKEY}
+STAT=$?
+checkStatus ${STAT} "${ASSOCLOAD}/bin/AssocLoad.sh"
+
+#
+#  Mark the file as having been processed successfully.
+#
+if [ ${RADAR_FLAG} -eq 1 ]
+then
+    echo "\n`date`" >> ${LOG_DIAG} 
+    echo "Mark file ${APP_INFILE} as processed" >> ${LOG_DIAG}
+    echo "command is:  ${RADAR_DBUTILS}/bin/logProcessedFile.csh ${RADAR_DBSCHEMADIR} ${JOBKEY} ${APP_INFILE} ABA"
+    ${RADAR_DBUTILS}/bin/logProcessedFile.csh ${RADAR_DBSCHEMADIR} ${JOBKEY} ${APP_INFILE} ABA >> ${LOG_DIAG} 2>&1
+    STAT=$?
+    if [ ${STAT} -ne 0 ]
+    then
+	echo "logProcessedFile.csh failed. Return status: ${STAT}" | tee -a ${LOG_DIAG} ${LOG_PROC}
+    fi
+fi
+
+#
+# run postload cleanup and email logs
+#
+shutDown
+echo "`date`" >> ${LOG}
+
+exit 0
---
+++ abaparse.py
@@ -0,0 +1,190 @@
+#!/usr/local/bin/python
+
+#
+# Program: abaparse.py
+#
+# Original Author: Lori E. Corbani
+#
+# Purpose:
+#
+# To generate an association loader input file from the ABA input file.
+#
+# Requirements Satisfied by This Program:
+#
+# Usage:  abamparse.py
+#
+# Envvars:
+#
+# Inputs:
+#
+# INFILE_NAME_ABA:  a file of ABA data
+#
+# Outputs:
+#
+# INFILE_NAME:  the output of this parser & the input file for the association loader
+#
+# Processing:
+#
+# Exit Codes:
+#
+# Assumes:
+#
+# Bugs:
+#
+# Implementation:
+#
+#    Modules:
+#
+# Modification History:
+#
+# 03/25/2008	lec
+#	- TR 8769
+#
+
+import sys
+import os
+import re
+import db
+import string
+import mgi_utils
+
+inFileName = os.environ['INFILE_NAME_ABA']
+assocFileName = os.environ['INFILE_NAME']
+
+diagFileName = os.environ['LOGDIR'] + '/abaparse.diagnostics'
+diagFile = ''		# file descriptor
+
+assocHeader = 'MGI\tABA\n'
+assocline = '%s\t%s\n'
+
+def exit(status, message = None):
+	#
+	# requires: status, the numeric exit status (integer)
+	#           message (string)
+	#
+	# effects:
+	# Print message to stderr and exits
+	#
+	# returns:
+	#
+ 
+    if message is not None:
+	sys.stderr.write('\n' + str(message) + '\n')
+ 
+    try:
+	diagFile.write('\n\nEnd Date/Time: %s\n' % (mgi_utils.date()))
+	diagFile.close()
+	inputerrorFile.close()
+	ens1errorFile.close()
+	ens2errorFile.close()
+	mgierrorFile.close()
+    except:
+	pass
+
+    try:
+	db.useOneConnection()
+    except:
+	pass
+
+    sys.exit(status)
+ 
+def init():
+	#
+	# requires: 
+	#
+	# effects: 
+	# 1. Processes command line options
+	# 2. Initializes global file descriptors/file names
+	#
+	# returns:
+	#
+ 
+    global diagFile, inputerrorFile, ens1errorFile, ens2errorFile, mgierrorFile
+    global mgiens
+ 
+    try:
+	diagFile = open(diagFileName, 'w')
+    except:
+	exit(1, 'Could not open file %s\n' % diagFileName)
+		
+    diagFile.write('Start Date/Time: %s\n\n' % (mgi_utils.date()))
+
+    db.useOneConnection(1)
+
+def writeAssoc(assocDict):
+	#
+	# requires:
+	#    assocDict (dictionary):  a key:value dictionary
+	#
+	# effects: 
+	# 1. Writes each association to the output association file
+	#
+	# returns:  nothing
+	#
+
+    try:
+	assocFile = open(assocFileName, 'w')
+    except:
+	exit(1, 'Could not open file %s\n' % assocFileName)
+		
+    assocFile.write(assocHeader)
+
+    for r in assocDict.keys():
+	for a in assocDict[r]:
+            assocFile.write(a)
+
+    assocFile.close()
+
+def process():
+	#
+	# requires:
+	#
+	# effects: 
+	#   input:  TreeFam input file
+	#   output: association loader file
+	#
+	# returns:  nothing
+	#
+
+    assocDict = {}
+
+    try:
+	inFile = open(inFileName, 'r')
+    except:
+	exit(1, 'Could not open file %s\n' % inFileName)
+		
+    lineNum = 0
+
+    for line in inFile.readlines():
+
+	if lineNum == 0:
+	    lineNum = lineNum + 1
+	    continue
+
+	s1 = re.sub('","', '|', line[:-1])
+	s2 = re.sub('"', '', s1)
+
+        tokens = string.split(s2, '|')
+
+	mgiID = tokens[5]
+	if mgiID == '':
+	    continue
+
+	if not assocDict.has_key(mgiID):
+	    assocDict[mgiID] = []
+	assocDict[mgiID].append(assocline % (mgiID, mgiID))
+
+        lineNum = lineNum + 1
+
+    writeAssoc(assocDict)
+
+    inFile.close()
+
+#
+# Main Routine
+#
+
+init()
+process()
+exit(0)
+
